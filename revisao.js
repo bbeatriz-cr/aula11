cc93c41140a419d812c8190876a926dbd47b5d9e
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-09-12 16:55:40 -0300

    contador

---
+++ revisao.js
@@ -0,0 +1,63 @@
+//let nome = "Zé"
+//let idade = 29
+//let altura = 1.70
+//let teamgato = true
+
+//alert("Olá"+nome+" tudo bem? ")
+//console.log(`Olá ${nome} tudo bem? Você tem ${idade} anos`)
+
+//let nome1 = prompt("digite seu nome:")
+//alert("O seu nome é "+nome1)
+
+//let numero1 = Number(prompt("Digite um número:"))
+//let numero2 = Number(prompt("Digite outro número:"))
+//let soma = numero1+numero2
+//alert("A soma foi de "+soma)
+
+//let dinheiro = 1.00
+//if(dinheiro > 5.00){
+   //alert("Posso comprar tal coisa")
+//}else if(dinheiro > 1.50) {
+    //alert("Uma bala")
+//}else {
+    //alert("Não posso comprar")
+//}
+
+//let n1 = Number(prompt("Digite a 1° nota:"))
+//let n2 = Number(prompt("Digite a 2° nota:"))
+//let n3 = Number(prompt("Digite a 3° nota:"))
+//let soma = n1+n2+n3
+//let media = soma/3
+//alert("A media foi de "+media)
+
+//let n1 = 20
+//n1 = 300
+//alert(n1)
+
+//const pi = 3.14
+//pi = 3.30
+//alert(pi)
+
+//let contador = 1  
+//while(contador <= 10){
+    //console.log(contador)
+    //contador++
+//}
+
+/*
+for(contador = 1; contador <= 10;contador++){
+    console.log(contador)
+}
+*/
+
+/*
+function ola(nome){
+    alert("Olá "+nome)
+}
+ola("Gabriel")
+ola("Andersson")
+ola("José")
+*/
+
+
+
